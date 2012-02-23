94c1dff4fdb1d18337e731ba530c4ca854b65607
Tom Cammann <tc191@hotmail.com>
2012-02-23 23:01:15 +0000

    Fixed file structure further

--- src/main/java/net/cammann/tom/fyp/core/EvolutionCycleListener.java
+++
@@ -1,9 +0,0 @@
-package net.cammann.tom.fyp.core;
-
-public interface EvolutionCycleListener {
-	
-	public void startCycle(EvolutionCycleEvent e);
-	
-	public void endCycle(EvolutionCycleEvent e);
-	
-}
